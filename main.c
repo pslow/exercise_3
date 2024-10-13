de01a980eb9a9bf24c312e61223ee200cabf5600
Pslow <mo.pr@outlook.com>
2024-10-13 15:17:44 +0200

    printed from 1 to 10 in terminal

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+<<<<<<< Updated upstream
 #include <stdio.h> 
+=======
+#include <stdio.h> 
+
+int main(void){
+    for (int i = 1; i < 11; i++){
+        printf("%d", i);
+    }
+    printf("\n");
+    return 0;
+}
+>>>>>>> Stashed changes
